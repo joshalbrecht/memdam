0b2d637b386dda20dfc27280c8e0a49369370921
Josh Albrecht <thejash@gmail.com>
2013-11-13 11:18:58 -0800

    emails are actually sent to a server

--- memdam/server/smtp_server.py
+++ memdam/server/smtp_server.py
@@ -13,11 +13,11 @@ class DebuggingServer(secure_smtpd.SMTPServer):
             None,
             require_authentication=True,
             ssl=True,
-            certfile='/home/cow/memdam/lib/secure-smtpd/examples/server.crt',
-            keyfile='/home/cow/memdam/lib/secure-smtpd/examples/server.key',
+            certfile='/home/cow/logproject/secure-smtpd/examples/server.crt',
+            keyfile='/home/cow/logproject/secure-smtpd/examples/server.key',
             credential_validator=secure_smtpd.FakeCredentialValidator(),
             process_count=None)
-    
+
     def process_message(self, peer, mailfrom, rcpttos, data):
         inheaders = 1
         lines = data.split('\n')
@@ -33,7 +33,7 @@ class DebuggingServer(secure_smtpd.SMTPServer):
 def main(listenAddress):
     DebuggingServer(listenAddress)
     asyncore.loop()
-    
+
 if __name__ == '__main__':
     #main(('0.0.0.0', 465))
     main(('0.0.0.0', 8465))
--- tests/integration/test_send_email.py
+++ tests/integration/test_send_email.py
@@ -1,10 +1,10 @@
 
 """Send a test email to the local server"""
 
-import sys
 import multiprocessing
 import asyncore
 import logging
+import time
 
 import memdam
 import memdam.common.time
@@ -18,23 +18,25 @@ DEVICE = "SuperTester"
 
 class EmailTestServer(memdam.server.smtp_server.DebuggingServer):
     """Simple test server. Validates the first message and exits immediately"""
-    def __init__(self, listenAddress):
+    def __init__(self, success_queue, listenAddress):
         memdam.server.smtp_server.DebuggingServer.__init__(self, listenAddress)
+        self.success_queue = success_queue
 
     def process_message(self, peer, mailfrom, rcpttos, data):
         """Called when the email is received. Validates that we got something reasonable"""
         #TODO: validate the email
-        sys.exit(0)
+        self.success_queue.put(True)
 
-def run_email_server():
+def run_email_server(success_queue):
     """Run from another process and actually processes the emails. Exit with code 0 if success"""
-    EmailTestServer(ADDRESS)
+    EmailTestServer(success_queue, ADDRESS)
     asyncore.loop()
 
 def test_send_and_receive_email():
     """Integration test. Starts email server and queue and makes sure that emails are received"""
+    success_queue = multiprocessing.Queue()
     #start the email server
-    process = multiprocessing.Process(target=run_email_server)
+    process = multiprocessing.Process(target=run_email_server, args=(success_queue,))
     process.start()
     #start the mail queue
     memdam.common.parallel.setup_log("email-integration", level=logging.DEBUG, handlers=[memdam.STDOUT_HANDLER])
@@ -47,10 +49,15 @@ def test_send_and_receive_email():
     message = memdam.recorder.message.Message(DEVICE, events)
     mail_queue.add_message(message)
     #check that it was received
-    process.join(5.0)
-    assert process.exitcode == 0
+    result = success_queue.get(True, 5.0)
+    time.sleep(2.0)
+    assert result
+    process.terminate()
     #shut everything down nicely
     mail_queue.shutdown()
+    success_queue.close()
+    success_queue.join_thread()
+    memdam.log.handlers[0]._shutdown()
 
 if __name__ == '__main__':
     test_send_and_receive_email()
