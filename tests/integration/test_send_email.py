a8fe0dab909c05b643c0712f5c81da71bcc0db9f
Josh Albrecht <thejash@gmail.com>
2013-11-13 10:51:06 -0800

    temp

--- tests/integration/test_send_email.py
+++ tests/integration/test_send_email.py
@@ -1,45 +1,56 @@
 
 """Send a test email to the local server"""
 
-import smtplib
-import email
-import email.encoders
-import email.message
-import email.mime.base
-import email.mime.multipart
-import email.mime.text
+import sys
+import multiprocessing
+import asyncore
+import logging
 
-def create_message(text_content, file_name=None):
-    message = email.mime.multipart.MIMEMultipart()
-    message['Subject'] = 'subject goes here'
-    message['To'] = 'toaddr'
-    message['From'] = 'fromaddr'
-    maintype = 'application'
-    subtype = 'octet-stream'
-    text_message = email.mime.text.MIMEText(text_content)
-    message.attach(text_message)
-    if file_name != None:
-        fp = open(file_name, "rb")
-        file_message = email.mime.base.MIMEBase(maintype, subtype)
-        file_message.set_payload(fp.read())
-        fp.close()
-        email.encoders.encode_base64(file_message)
-        message.add_header('Content-Disposition', 'attachment', filename=file_name)
-        message.attach(file_message)
-    composed = message.as_string()
-    return composed
+import memdam
+import memdam.common.time
+import memdam.common.event
+import memdam.recorder.message
+import memdam.recorder.mailqueue
+import memdam.server.smtp_server
 
-def main():
-    fromaddr = 'user_me@gmail.com'
-    toaddrs  = 'user_you@gmail.com'
-    msg = create_message('Why,Oh why!', __file__)
-    username = 'bcoe'
-    password = 'foobar'
-    server = smtplib.SMTP_SSL('127.0.0.1:8465')
-    server.login(username,password)
-    server.sendmail(fromaddr, toaddrs, msg)
-    server.quit()
-    
+ADDRESS = ('127.0.0.1', 8465)
+DEVICE = "SuperTester"
+
+class EmailTestServer(memdam.server.smtp_server.DebuggingServer):
+    """Simple test server. Validates the first message and exits immediately"""
+    def __init__(self, listenAddress):
+        memdam.server.smtp_server.DebuggingServer.__init__(self, listenAddress)
+
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        """Called when the email is received. Validates that we got something reasonable"""
+        #TODO: validate the email
+        sys.exit(0)
+
+def run_email_server():
+    """Run from another process and actually processes the emails. Exit with code 0 if success"""
+    EmailTestServer(ADDRESS)
+    asyncore.loop()
+
+def test_send_and_receive_email():
+    """Integration test. Starts email server and queue and makes sure that emails are received"""
+    #start the email server
+    process = multiprocessing.Process(target=run_email_server)
+    process.start()
+    #start the mail queue
+    memdam.common.parallel.setup_log("email-integration", level=logging.DEBUG, handlers=[memdam.STDOUT_HANDLER])
+    mail_queue = memdam.recorder.mailqueue.MailQueue(4, ["someone@somewhere.com"], ADDRESS)
+    #send an email
+    events = [
+        memdam.common.event.Event(memdam.common.time.now(), some_text="asdfsd", x_text="d"),
+        memdam.common.event.Event(memdam.common.time.now(), some_text="tryr", x_text="g98f"),
+    ]
+    message = memdam.recorder.message.Message(DEVICE, events)
+    mail_queue.add_message(message)
+    #check that it was received
+    process.join(5.0)
+    assert process.exitcode == 0
+    #shut everything down nicely
+    mail_queue.shutdown()
+
 if __name__ == '__main__':
-    main()
-    
+    test_send_and_receive_email()
