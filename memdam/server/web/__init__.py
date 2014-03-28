4f3ddb7e349d4200b88f3df03eda0624b972acd5
Josh Albrecht <thejash@gmail.com>
2014-03-28 10:13:13 -0700

    now logs configuration at startup for my own sanity...

--- memdam/server/web/__init__.py
+++ memdam/server/web/__init__.py
@@ -7,6 +7,8 @@ app.config.update(dict(
     BLOBSTORE_FOLDER='/tmp',
     DEBUG=True,
     SECRET_KEY='development key',
-    RUN_WSGI_SERVER=False
+    RUN_WSGI_SERVER=False,
+    LISTEN_ADDRESS='127.0.0.1',
+    LISTEN_PORT=5000,
 ))
 #see web_server.py for the logic about which settings are overriden in which order
--- memdam/server/web_server.py
+++ memdam/server/web_server.py
@@ -11,26 +11,52 @@ from funcy import *
 import memdam.server.admin
 import memdam.server.web.urls
 
-DEFAULTS = {
-    'LISTEN_ADDRESS': '127.0.0.1',
-    'LISTEN_PORT': 5000
-}
-
-def _load_config_from_file(config_file):
-    """Loads the configuration file if defined"""
+def _load_config_from_file(config_file, config_source):
+    '''Loads the configuration file if defined'''
     if config_file != None:
-        assert os.path.exists(config_file), "Config file (%s) does not exist!" % (config_file)
+        assert os.path.exists(config_file), 'Config file (%s) does not exist!' % (config_file)
         memdam.server.web.urls.app.config.from_pyfile(config_file, silent=False)
+        _update_config_source(config_source, memdam.server.web.app.config, 'FILE=' + config_file)
+
+def _update_config_source(source, config, name):
+    '''
+    :param source: a dictionary tracking both the current config value, and where it came from.
+    :type  source: dict(string -> (string(value), string(source)))
+    :param config: the web app configuration
+    :type  config: ???
+    :param name: the name of the source that most recently updated config
+    :type  name: string
+    '''
+    for key in config:
+        if key in source:
+            if source[key][0] != config[key]:
+                source[key] = (config[key], name)
+        else:
+            source[key] = (config[key], name)
+
+def _format_config_source(source):
+    '''
+    :returns: string (describing the source and value of each configuration variable)
+    '''
+    formatted_values = '\n'.join(['    %s=%s (from %s)' % (key, source[key][0], source[key][1]) \
+                                  for key in sorted(source.keys())])
+    return 'Current Configuration:\n' + formatted_values
 
 def _parse_config(kwargs):
-    #configuration starts with defaults as defined above
-    memdam.server.web.app.config.update(copy.deepcopy(DEFAULTS))
+    #track which configuration came from where for our own sanity:
+    config_source = {}
+    _update_config_source(config_source, memdam.server.web.app.config, 'DEFAULT')
+
     #if the name of a config file was passed in programmatically, load those settings
-    _load_config_from_file(kwargs.get('CONFIG_FILE', None))
+    _load_config_from_file(kwargs.get('CONFIG_FILE', None), config_source)
     #override anything defined via the kwargs
     memdam.server.web.app.config.update(kwargs)
+    _update_config_source(config_source, memdam.server.web.app.config, 'KWARGS')
     #if the name of a config file was defined in an environment variable, load those settings
-    _load_config_from_file(os.environ.get('YOURAPPLICATION_SETTINGS', None))
+    _load_config_from_file(os.environ.get('YOURAPPLICATION_SETTINGS', None), config_source)
+
+    print(_format_config_source(config_source))
+    #TODO: maybe log also, right after applying settings to log?
 
     #fix up the logger
     memdam.log = memdam.server.web.urls.app.logger
@@ -51,12 +77,12 @@ def _run_server():
         memdam.server.web.urls.app.run(debug=True, use_reloader=False)
 
 def run(**kwargs):
-    """Parses configuration and runs the server"""
+    '''Parses configuration and runs the server'''
     _parse_config(kwargs)
     _run_server()
 
 def test_run(username, password, **kwargs):
-    """Same as run, except that it also creates a user. Useful for testing."""
+    '''Same as run, except that it also creates a user. Useful for testing.'''
     _parse_config(kwargs)
     memdam.server.admin.create_archive(username, password)
     _run_server()
@@ -86,7 +112,7 @@ def read_commandline_args():
     return defined_args
 
 def run_as_script():
-    """Parses commandline arguments, converting them into the appropriate config variables"""
+    '''Parses commandline arguments, converting them into the appropriate config variables'''
     run(**read_commandline_args())
 
 if __name__ == '__main__':
